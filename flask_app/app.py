f8c42fa9b55996963003275d69422a01acff2829
Daniel <prayson.wilfred.daniel@if.dk>
2018-07-26 17:21:52 +0200

    add top examples

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,7 +1,31 @@
+import sys
+sys.path.insert(0, '../notebooks/')
 from flask import Flask, render_template, request
 from sklearn.externals import joblib
+
+
+
+clf = joblib.load('../SGDclassifier.pkl')
+hash_vec = joblib.load('../HashVectorizer.pkl')
+
+X = hash_vec.transform(['jeg elsker pizze'])
+y = clf.predict_proba(X)
+print(X,y,clf.classes_)
 
 ## load stops tokenize and model
 app = Flask(__name__)
 
-pass
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    
+    return render_template('home.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+
+    return render_template('result.html', in_review='hej you',in_response='Positive')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
